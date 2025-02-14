7c1441a3aebb2a8556affe166b346a0a586efc47
Leandro Rodrigues <lj251004@gmail.com>
2025-02-14 13:07:18 -0300

    feat: enhance webhook GET handler to log search parameters and process API response

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -4,6 +4,8 @@ export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
 
+    console.log("Search Params: ", searchParams);
+
     const email = searchParams.get("email");
     const id = searchParams.get("id");
 
@@ -14,13 +16,37 @@ export async function GET(req: Request) {
       );
     }
 
-    console.log("Webhook recebido:", { email, id });
+    console.log("Webhook recebido: ", { email, id });
 
-    // salvar no banco de dados
+    // Enviar POST para API de exemplo
+    const response = await fetch(
+      "https://backend.testeswaffle.org/webhooks/case/subscribe",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
 
-    return NextResponse.json({ message: "Webhook recebido com sucesso" });
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: "Falha ao notificar a API" },
+        { status: 500 }
+      );
+    }
+
+    const data = await response.json();
+
+    console.log("Resposta da API: ", data);
+
+    return NextResponse.json(
+      { message: "Webhook processado com sucesso", data },
+      { status: 200 }
+    );
   } catch (error) {
-    console.error("Erro no webhook:", error);
+    console.error("Erro no webhook: ", error);
     return NextResponse.json({ error: "Erro interno" }, { status: 500 });
   }
 }
